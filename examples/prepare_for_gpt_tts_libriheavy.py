d521cb2e04af5d79c16e68706e350b5de7d55e71
Wataru Nakata <wataru9871@gmail.com>
2024-02-11 20:39:48 +0900

    updated script

--- examples/prepare_for_gpt_tts_libriheavy.py
+++ examples/prepare_for_gpt_tts_libriheavy.py
@@ -9,24 +9,32 @@ from phonemizer.backend import EspeakBackend
 from phonemizer.logger import get_logger
 from tqdm import tqdm
 import random
+import tempfile
 
 unique_phones = set()
 
+def divide_chunks(l, n): 
+    # looping till length l 
+    for i in range(0, len(l), n):  
+        yield l[i:i + n] 
 @hydra.main(config_path='config', config_name='config',version_base=None)
 def prepare_dataset_as_json(cfg):
     output_stream = Path(cfg.ulm.librilight.output_jsonl_path).open('w')
     backend = EspeakBackend('en-us',logger=get_logger(verbosity='quiet'),words_mismatch='ignore')
-    speeches = dict()
-    features = dict()
-    speech_by_speaker = dict()
-    for manifest_path in Path(cfg.ulm.librilight.manifest_root).glob(cfg.ulm.librilight.manifest_pattern):
+    manifest_paths = list(Path(cfg.ulm.librilight.manifest_root).glob(cfg.ulm.librilight.manifest_pattern))
+    for sub_paths in tqdm(divide_chunks(manifest_paths,10)):
+        speech_by_speaker = dict()
         feature_paths = list(Path(cfg.ulm.librilight.feature_root).glob(cfg.ulm.librilight.feature_pattern))
-        feature_path = [x for x in feature_paths if manifest_path.stem.split('.')[0] in x.stem][0]
-        with open(feature_path) as f:
-            lines = [l.strip() for l in f.readlines()]
-            for line in lines:
-                features[line.split('|')[0].strip()] = line.split('|')[1].strip().split()
-        manifest: lhotse.CutSet= lhotse.load_manifest(manifest_path)
+        feature_paths = [x for x in feature_paths if x.stem.split('.')[0] in [y.stem.split('.')[0] for y in sub_paths]]
+        features = dict()
+        for feature_path in feature_paths:
+            with open(feature_path) as f:
+                lines = [l.strip() for l in f.readlines()]
+                for line in lines:
+                    features[line.split('|')[0].strip()] = line.split('|')[1].strip().split()
+        manifest = lhotse.load_manifest(str(sub_paths[0]))
+        for manifest_path in sub_paths[1:]:
+            manifest: lhotse.CutSet=  manifest + lhotse.load_manifest(manifest_path) 
         for x in tqdm(manifest):
             wav_path = f"{x.id}.flac"
             normalized_text = x.supervisions[0].custom['texts'][0]
@@ -47,25 +55,25 @@ def prepare_dataset_as_json(cfg):
                 'data': data,
                 'speaker': speaker
             })
-    for speaker in tqdm(speech_by_speaker.keys()):
-        for speech in speech_by_speaker[speaker]:
-            for i in range(10):
-                same_speaker_speeches = speech_by_speaker[speaker]
-                if len(same_speaker_speeches) == 1:
-                    continue
-                random_speech = random.choice(same_speaker_speeches)
-                while random_speech['wav_path'] == speech['wav_path']:
+        for speaker in tqdm(speech_by_speaker.keys()):
+            for speech in speech_by_speaker[speaker]:
+                for i in range(10):
+                    same_speaker_speeches = speech_by_speaker[speaker]
+                    if len(same_speaker_speeches) == 1:
+                        continue
                     random_speech = random.choice(same_speaker_speeches)
-                prompt_data = random_speech['data']
-                prompt_phones = random_speech['phones']
-                data = speech['data']
-                phones = speech['phones']
-                output_stream.write(
-                    json.dumps(
-                        {'text':f"<s> {' '.join(list(prompt_phones))} {' '.join(list(phones))} [SEP] {' '.join(prompt_data)} {' '.join(data)} </s>"}
-                    ,ensure_ascii=False)
-                )
-                output_stream.write('\n')
+                    while random_speech['wav_path'] == speech['wav_path']:
+                        random_speech = random.choice(same_speaker_speeches)
+                    prompt_data = random_speech['data']
+                    prompt_phones = random_speech['phones']
+                    data = speech['data']
+                    phones = speech['phones']
+                    output_stream.write(
+                        json.dumps(
+                            {'text':f"<s> {' '.join(list(prompt_phones))} {' '.join(list(phones))} [SEP] {' '.join(prompt_data)} {' '.join(data)} </s>"}
+                        ,ensure_ascii=False)
+                    )
+                    output_stream.write('\n')
     output_stream.close()
     return unique_phones
 
